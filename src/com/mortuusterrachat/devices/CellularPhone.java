9736096f89adba68cee63c692facfd624a9445cf
joseph <kadeska23@gmail.com>
2017-10-22 14:50:11 -0700

    Added notice for Licence

--- src/com/mortuusterrachat/devices/CellularPhone.java
+++ src/com/mortuusterrachat/devices/CellularPhone.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2017 Mortuss Terra Team
+ * You should have received a copy of the GNU General Public License along with this program. 
+ * If not, see <https://github.com/kadeska/MT_Communication/blob/master/LICENSE>.
+ */
 package com.mortuusterrachat.devices;
 
 import java.util.List;
