796820e2f0f1aabdba0d1c4d96219beac506d1e9
joseph <kadeska23@gmail.com>
2017-10-29 16:32:59 -0700

    fixed some issues with bin folder

--- src/com/mortuusterrachat/managers/MessageScrambler.java
+++ src/com/mortuusterrachat/managers/MessageScrambler.java
@@ -8,102 +8,83 @@ package com.mortuusterrachat.managers;
 import java.util.Random;
 
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-
-import com.mortuusterrachat.MortuusTerraChat;
 
 public class MessageScrambler {
 
-  private MortuusTerraChat main = JavaPlugin.getPlugin(MortuusTerraChat.class);
+	final int MIN_DISTANCE = 0;
+	final int MAX_DISTANCE = 200;
+	final int EIGHTY_PERCENT = MAX_DISTANCE * 80 / 100;
+	final int SIXTY_PERCENT = MAX_DISTANCE * 60 / 100;
+	final int FORTY_PERCENT = MAX_DISTANCE * 40 / 100;
+	final int TWENTY_PERCENT = MAX_DISTANCE * 20 / 100;
 
-  final int MIN_DISTANCE = 0;
-  final int MAX_DISTANCE = 100;
-  final int EIGHTY_PERCENT = MAX_DISTANCE * 80 / 100;
-  final int SIXTY_PERCENT = MAX_DISTANCE * 60 / 100;
-  final int FORTY_PERCENT = MAX_DISTANCE * 40 / 100;
-  final int TWENTY_PERCENT = MAX_DISTANCE * 20 / 100;
+	final Random ran = new Random();
 
-  final Random ran = new Random();
+	public String scramble(String message, Player from, Player to) {
 
-  public String scramble(String message, Player from, Player to) {
+		int distance = (int) from.getLocation().distance(to.getLocation());
 
-    int distance = (int) from.getLocation().distance(to.getLocation());
+		// Too far away, send nothing
+		if (distance >= MAX_DISTANCE) {
+			return null;
+		}
 
-    // Too far away, send nothing
-    if (distance >= MAX_DISTANCE) {
-      main.getLogger().info("Distance over 100");
-      return null;
-    }
+		// Distance between 199 - 160
+		if (MAX_DISTANCE > distance && distance >= EIGHTY_PERCENT) {
+			return obfuscate(message, 8);
+		}
 
-    // Distance between 99 - 80
-    if (MAX_DISTANCE > distance && distance >= EIGHTY_PERCENT) {
-      main.getLogger().info("Distance between 99 - 80");
+		// Distance between 159 - 120
+		if (EIGHTY_PERCENT > distance && distance >= SIXTY_PERCENT) {
+			return obfuscate(message, 6);
+		}
 
-      return obfuscate(message, 8);
-    }
+		// Distance between 119 - 80
+		if (SIXTY_PERCENT > distance && distance >= FORTY_PERCENT) {
+			return obfuscate(message, 4);
+		}
 
-    // Distance between 79 - 60
-    if (EIGHTY_PERCENT > distance && distance >= SIXTY_PERCENT) {
-      main.getLogger().info("Distance between 79 - 60");
+		// Distance between 79 - 40
+		if (FORTY_PERCENT > distance && distance >= TWENTY_PERCENT) {
+			return obfuscate(message, 2);
+		}
 
-      return obfuscate(message, 6);
-    }
+		// Distance less than 40. Message clear
+		if (distance < TWENTY_PERCENT) {
+			return message;
+		}
+		return message;
+	}
 
-    // Distance between 59 - 40
-    if (SIXTY_PERCENT > distance && distance >= FORTY_PERCENT) {
-      main.getLogger().info("Distance between 59 - 40");
+	private String obfuscate(String toObfuscate, int amount) {
+		// Get rid of the blank spaces
+		String[] temp = toObfuscate.split(" ");
+		String noBlanks = "";
 
-      return obfuscate(message, 4);
-    }
+		for (String s : temp)
+			noBlanks += s;
 
-    // Distance between 39 - 20
-    if (FORTY_PERCENT > distance && distance >= TWENTY_PERCENT) {
-      main.getLogger().info("Distance between 39 - 20");
+		if (amount > noBlanks.length())
+			amount = noBlanks.length() - 1;
 
-      return obfuscate(message, 2);
-    }
+		int index;
+		char[] charsToReplace = new char[amount];
 
-    // Distance less than 20. Message clear
-    if (distance < TWENTY_PERCENT) {
-      main.getLogger().info("Distance less 20");
+		for (int i = 0; i < amount; i++) {
+			index = ran.nextInt(noBlanks.length());
+			charsToReplace[i] = noBlanks.charAt(index);
+		}
 
-      return message;
-    }
+		String obfuscated = toObfuscate;
 
-    main.getLogger().info("nothing");
+		final String[] replacements = { "&k%&r", "&k%&r", "&l!&r", "&m#&r", "&n%&r", "&o%&r", "$", "@", "!", "?", "ö",
+				"*", "ä", "ü" };
 
-    return message;
-  }
+		for (Character c : charsToReplace) {
+			obfuscated = obfuscated.replace(String.valueOf(c), replacements[ran.nextInt(replacements.length)]);
+		}
 
-  private String obfuscate(String toObfuscate, int amount) {
-    // Get rid of the blank spaces
-    String[] temp = toObfuscate.split(" ");
-    String noBlanks = "";
-    
-    for (String s : temp)
-      noBlanks += s;
-    
-    if (amount > noBlanks.length())
-      amount = noBlanks.length() - 1;
+		return obfuscated;
 
-    int index;
-    char[] charsToReplace = new char[amount];
-
-    for (int i = 0; i < amount; i++) {
-      index = ran.nextInt(noBlanks.length());
-      charsToReplace[i] = noBlanks.charAt(index);
-    }
-    
-    String obfuscated = toObfuscate;
-
-    final String[] replacements = { "&k%&r", "&k%&r", "&l!&r", "&m#&r", "&n%&r", "&o%&r", "$", "@", "!", "?", "ö", "*", "ä", "ü"};
-    
-    for (Character c : charsToReplace) {
-      main.getLogger().info("Selected char " + c);
-      obfuscated = obfuscated.replace(String.valueOf(c), replacements[ran.nextInt(replacements.length)]);
-    }
-
-    return obfuscated;
-
-  }
+	}
 }
